f1760786eec1c1c2a75b30bf9f213b8891812e6d
VOC12 <2109155@upy.edu.mx>
2022-05-26 22:16:01 -0500

    add all codes

---
+++ assigmentmatriz.cpp
@@ -0,0 +1,24 @@
+/*          Assigment: Make a program to fill a matrix asking to the user the number of rows and columns.
+            
+            Then print the matrix in terminal
+            
+            Then print the tranpose of the matrix
+            
+            A matrix is tanspose if the column i was was the row in the original matrix
+
+            123		147
+            456	-->	258
+            789		369 
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout >> "Matriz Generator. //n  Plase, introduce "
+
+
+
+}
